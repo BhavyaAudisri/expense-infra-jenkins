0bbb706ca32a41784280fb461ebdf9ded2c37493
BhavyaAudisri <somisetty.bhavya@gmail.com>
2025-04-11 13:06:39 +0530

    updated code

--- 00-vpc/provider.tf
+++ 00-vpc/provider.tf
@@ -7,15 +7,13 @@ terraform {
     }
   }
 
-  backend "s3" {
-    bucket         = "somi-jenkins-infra"
-    key            = "tf-jenkins-vpc" # you should have unique keys with in the bucket, same key should not be used in other repos or tf projects
-    region         = "us-east-1"
-    dynamodb_table = "somi-jenkins-infra"
+ backend "s3" {
+    bucket  = "somi-jenkins-infra"
+    key     = "jenkins-vpc"
+    region  = "us-east-1"
+    encrypt = true
+    use_locking {
+      table = "somi-jenkins-infra"
+    }
   }
-}
-
-provider "aws" {
-  # Configuration options
-  region = "us-east-1"
 }
