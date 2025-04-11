43f09b18af6f848cfe1be08dc79df0d3f590b954
BhavyaAudisri <somisetty.bhavya@gmail.com>
2025-04-11 13:11:24 +0530

    updated code

--- 00-vpc/provider.tf
+++ 00-vpc/provider.tf
@@ -3,16 +3,15 @@ terraform {
     aws = {
       source  = "hashicorp/aws"
       version = "5.84.0"
-      #version = "~> 5.84"
+      # version = "~> 5.84"
     }
   }
 
- backend "s3" {
-    bucket  = "somi-jenkins-infra"
-    key     = "jenkins-vpc"
-    region  = "us-east-1"
-    encrypt = true
-    use_locking = true
-    }
+  backend "s3" {
+    bucket         = "somi-jenkins-infra"
+    key            = "jenkins-vpc"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "somi-jenkins-infra"  # Enables state locking
   }
 }
