87243b11eb9cdd4aa258440236d444c114b9e4ad
BhavyaAudisri <somisetty.bhavya@gmail.com>
2025-04-11 18:52:14 +0530

    vpc

--- 00-vpc/provider.tf
+++ 00-vpc/provider.tf
@@ -3,14 +3,19 @@ terraform {
     aws = {
       source  = "hashicorp/aws"
       version = "5.84.0"
-      # version = "~> 5.84"
+      #version = "~> 5.84"
     }
   }
 
   backend "s3" {
     bucket         = "somi-jenkins-infra"
-    key            = "jenkins-vpc"
+    key            = "jenkins-vpc-test" # you should have unique keys with in the bucket, same key should not be used in other repos or tf projects
     region         = "us-east-1"
-    dynamodb_table = "somi-jenkins-infra"  # Enables state locking
+    dynamodb_table = "somi-jenkins-infra"
   }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
 }
